97d20fbf067ca34960bd0c43de156b07b9f474db
martinico83 <martin.nicolas.os83@gmail.com>
2022-10-15 17:16:25 -0300

    Haciendo Ej 7 Unidad 2

---
+++ IFTS24/Unidad_2/Ejercicio_7/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Declarar dos vectores de 5 elementos cada uno (v1 y v2) con valores predefinidos. Recorrerlos y
+mostrar por pantalla la multiplicación de los valores de cada vector EJ: cout<< V1[0] * v2[0]
+ * */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
